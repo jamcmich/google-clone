2420812f7a230c830e83d9e08644f81ea775a871
Jacob McMichael <jacobmcmichael@gmail.com>
2022-07-12 13:29:00 -0700

    Installed react-router-dom package

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 	return (
 		<div className='home__wrapper'>
-			<div className='home__header'>Home Header</div>
-			<div className='home__body'>Home Body</div>
+			<div className='home__header'>
+				<div className='home__header_left'>
+					<Link to='/about'>About</Link>
+					<Link to='/store'>Store</Link>
+				</div>
+
+				<div className='home__header_right'>
+					<Link to='/gmail'>Gmail</Link>
+					<Link to='/images'>Images</Link>
+				</div>
+			</div>
+			<div className='home__body'></div>
 		</div>
 	);
 };
