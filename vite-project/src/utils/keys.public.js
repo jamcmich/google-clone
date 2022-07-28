d065937399cae7695c49103630e296c3063bbe73
Jacob McMichael <jacobmcmichael@gmail.com>
2022-07-27 18:15:59 -0700

    Added a public API key for deployment

---
+++ vite-project/src/utils/keys.public.js
@@ -0,0 +1,3 @@
+const API_KEY = '3f2d81e22emsh8d8c990d958af88p1e6b8bjsn59bbdacf97ea';
+
+export { API_KEY };
--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/google-search-clone/',
 	plugins: [react(), WindiCSS()],
 	resolve: {
 		alias: {
