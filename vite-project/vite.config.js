8ea165bb806f8a3d75cc688f91a3d8a3ca2a7595
Jacob McMichael <jacobmcmichael@gmail.com>
2022-07-27 19:22:11 -0700

    Amended base in vite.config.js to match "/" path

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/google-search-clone/',
+	base: '/',
 	plugins: [react(), WindiCSS()],
 	resolve: {
 		alias: {
